f9f76249f1438b15ff83f33f51d95813532cd35e
YoonJaeil <fsfinalz@gmail.com>
2016-10-24 16:14:30 +0900

    Added CollectionView and images

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by YoonJae Il on 2016. 10. 24..
+//  Copyright © 2016년 YoonJae Il. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by YoonJae Il on 2016. 10. 23..
+//  Copyright © 2016년 YoonJae Il. All rights reserved.
+//
+
+import Foundation
